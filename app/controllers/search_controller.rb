decd1186175d6a0c23c77adda32f80fea8529406
Dibyo Mukherjee <vagrant@heroku.(none)>
2014-01-18 22:49:05 +0100

    Remove tokens

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,7 @@ class SearchController < ApplicationController
 
 	def index
 		require 'rdio'		
-		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'], 
-                    ['5fxyvarsbr87dr3xs8u8x3wf', 'VGSHB9jm7MhG'])
+		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'])
 		@search = rdio.call('search', {query: 'Dark Horse', types:'Track'})['result']['results']
 
 		render :json => @tweets
