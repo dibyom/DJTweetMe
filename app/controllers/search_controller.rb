f99cd8489c6882221149defd577bd18e4bdac00f
Dibyo Mukherjee <vagrant@heroku.(none)>
2014-01-18 22:55:33 +0100

    Fix stupid typo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'])
 		@search = rdio.call('search', {query: 'Dark Horse', types:'Track'})['result']['results']
 
-		render :json => @tweets
+		render :json => @search
 	end
 	
 
