cfff6615fea9681d70e8d876b16f62861f8d45a1
Dibyo Mukherjee <vagrant@heroku.(none)>
2014-01-18 22:44:23 +0100

    Change key

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
 	def index
 		require 'rdio'		
 		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'], 
-                    ['gtkm2nkemvm2z62kqzcdretf', 'bHXTWsNWBXRE'])
+                    ['5fxyvarsbr87dr3xs8u8x3wf', 'VGSHB9jm7MhG'])
 		@search = rdio.call('search', {query: 'Dark Horse', types:'Track'})['result']['results']
 
 		render :json => @tweets
