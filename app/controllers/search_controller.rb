7a18fe20d364c690f678bc079c9624a9a25fd8e5
Dibyo Mukherjee <vagrant@heroku.(none)>
2014-01-18 23:00:22 +0100

    Add params to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,8 @@ class SearchController < ApplicationController
 	def index
 		require 'rdio'		
 		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'])
-		@search = rdio.call('search', {query: 'Dark Horse', types:'Track'})['result']['results']
+
+		@search = rdio.call('search', {query: param[:query], types:'Track'})['result']['results']
 
 		render :json => @search
 	end
