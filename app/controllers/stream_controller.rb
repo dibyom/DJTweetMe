c243162951bee490e713a789b7df0fd0cd47417a
Dibyo Mukherjee <vagrant@heroku.(none)>
2014-01-18 18:49:53 +0100

    Add index stream

---
+++ app/controllers/stream_controller.rb
@@ -0,0 +1,8 @@
+class StreamController < ApplicationController
+	respond_to :json
+
+	def index
+		@tweets = Tweet.all
+		render :json => @tweets
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ DjtweetServer::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+   root 'Stream#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
