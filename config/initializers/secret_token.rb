05adeb9c4c972a65cc5a1d1369a74ed454186725
vagrant <vagrant@heroku.(none)>
2014-01-18 17:23:13 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DjtweetServer::Application.config.secret_key_base = '03532152340ad037ea6d97145b1903f40f8c3abb30cf2f0045f0905c8630ee32884febf0e3bf6be9e43d1a2f90a01d3a4adca104c756406f862ff2ca9d0fa9ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DjtweetServer::Application.config.session_store :cookie_store, key: '_djtweet-server_session'
