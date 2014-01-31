aabd67dd85fb7f1fe1422743a21af95df63f0c44
Dibyajyoti Mukherjee <dibyo.m@gmail.com>
2014-01-31 13:07:30 -0500

    Typo

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 	def index
 		require 'rdio'	
 
-		rdio = Rdio.new([ENV["RDIO KEY"], ENV["RDIO SECRET"]])
+		rdio = Rdio.new([ENV["RDIO_KEY"], ENV["RDIO_SECRET"]])
 		@search = rdio.call('search', {query: params[:query], types:'Track'})['result']['results']
 		render :json => @search
 	end
