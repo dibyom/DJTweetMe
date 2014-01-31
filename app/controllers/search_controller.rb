2d042ee5c4d42ba2a8e28d3e4802e87262a5a010
Dibyajyoti Mukherjee <dibyo.m@gmail.com>
2014-01-31 13:04:41 -0500

    Env variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 	def index
 		require 'rdio'	
 
-		rdio = Rdio.new(['62k6xnjstf92y6zcmfqd5ajj', 'ZY5XxvFXqe'])
+		rdio = Rdio.new([ENV["RDIO KEY"], ENV["RDIO SECRET"]])
 		@search = rdio.call('search', {query: params[:query], types:'Track'})['result']['results']
 		render :json => @search
 	end
